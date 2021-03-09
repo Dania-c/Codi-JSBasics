b9f2663151596c70f625ad8663e394813aac82c8
dania-c <workingdania@hotmail.com>
2021-03-09 21:09:25 +0200

    Basic03 => index.HTML + Step_1.js

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,9 @@
+function addborder(){
+  
+    document.getElementById('image1').style.border = "3px solid red";
+}
+
+function removeborder(){
+  
+    document.getElementById('image1').style.border = "none";
+}
