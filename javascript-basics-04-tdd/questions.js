fc5bbd68aab3bd130d82dcd4c865191687c392eb
dania-c <workingdania@hotmail.com>
2021-03-10 12:57:26 +0200

    Basic04 => createLanguagesArray done

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,71 +1,75 @@
 
 const stringSize = (text) => {
-
+    return text.length;
 }
 const replaceCharacterE = (text) => {
-
+    return text.replace("e", " ");
 }
 const concatString = (text1, text2) => {
-
+    return text1.concat(text2);
 }
 const showChar5 = (text) => {
-
+    
+    return text.charAt(4);
 }
 const showChar9 = (text) => {
-
+    return text.substr(0,9);
 }
 const toCapitals = (text) => {
-
+    return text.toUpperCase();
 }
 const toLowerCase = (text) => {
-
+    return text.toLowerCase();
 }
 const removeSpaces = (text) => {
-
+    return text.trim();
 }
 const IsString = (text) => {
-
+    return text.IsString=true ;
 }
 
 const getExtension = (text) => {
+   // return text.substring(text.search("."));
+   return text.split(".").pop();
 
 }
 const countSpaces = (text) => {
-
+    return text.split(" ").length-1;
 }
 const InverseString = (text) => {
-
+    return text.split("").reverse().join("");
 }
 
 const power = (x, y) => {
-
+return x**y;
 }
 const absoluteValue = (num) => {
-
+return Math.abs(num);
 }
 const absoluteValueArray = (array) => {
-
+return array.map(Math.abs);
 }
 const circleSurface = (radius) => {
-
+    return Math.round(Math.PI * radius * radius);  
 }
 const hypothenuse = (ab, ac) => {
-
+    return Math.hypot(ab, ac);
 }
 const BMI = (weight, height) => {
-
+    return parseFloat((weight / (height * height)).toFixed(2));
 }
 
 const createLanguagesArray = () => {
-
+    const languages = ["Html", "CSS", "Java", "PHP"];
+    return languages;
 }
 
 const createNumbersArray = () => {
-
+    
 }
 
 const replaceElement = (languages) => {
-
+    
 }
 
 const addElement = (languages) => {
@@ -73,15 +77,15 @@ const addElement = (languages) => {
 }
 
 const addNumberElement = (numbers) => {
-
+    
 }
 
 const removeFirst = (languages) => {
-
+    
 }
 
 const removeLast = (languages) => {
-
+   
 }
 
 const convertStrToArr = (social_arr) => {
@@ -93,9 +97,9 @@ const convertArrToStr = (languages) => {
 }
 
 const sortArr = (social_arr) => {
-
+   
 }
 
 const invertArr = (social_arr) => {
-
+   
 }
