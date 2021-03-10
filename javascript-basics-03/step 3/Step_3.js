e846f2bf4980e488b0443356ea64ddc2010b7bbc
dania-c <workingdania@hotmail.com>
2021-03-10 08:48:58 +0200

    Basic03 =>  index.html + Step_3.js

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,30 @@
+function changeColorG(){
+  
+    // var greenColor = document.getElementsByClassName("color green");
+    // var redColor = document.getElementsByClassName("color red");
+    // var blueColor = document.getElementsByClassName("color blue");
+    var divTag = document.getElementById("text");
+    divTag.style.color="green";
+
+//     if (classColor=="color green") {
+//         divTag.display.color ="green";
+//     }
+   
+//     var x=document.activeElement;//x=[object HTMLBodyElement].  greencolor=[object HTMLCollection]
+//    alert (x);
+    
+// if (document.activeElement.className. === greenColor) {
+//     alert("green");
+//     divTag.style.color="green";}
+// else if( document.activeElement.className=== redColor) {divTag.style.display="red";}
+
+}
+function changeColorR(){
+    var divTag = document.getElementById("text");
+    divTag.style.color="red";
+}
+function changeColorB(){
+    var divTag = document.getElementById("text");
+    divTag.style.color="blue";
+}
+
